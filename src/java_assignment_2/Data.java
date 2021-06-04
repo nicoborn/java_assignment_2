018cee0b3e58c7352e4b68357b0a3a7f430f4f8b
Nico Born <born.nico@gmail.com>
2021-06-04 12:03:35 +0200

    Read CSV

--- src/java_assignment_2/Data.java
+++ src/java_assignment_2/Data.java
@@ -1,5 +1,88 @@
 package java_assignment_2;
 
-public class Data {
+import java.util.Date;
 
+public class Data {
+	
+	// Variables
+	private String date;
+	private float eurchf;
+	private float gbpchf;
+	private float usdchf;
+	private float jpychf;
+	private float audchf;
+	private float xdrchf;
+	
+	protected Data() {
+		date = "";
+		eurchf = 0;
+		gbpchf = 0;
+		usdchf = 0;
+		jpychf = 0;
+		audchf = 0;
+		xdrchf = 0;
+	}
+	
+	// Date
+	public String getDate() {
+	    return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	// EUR / CHF
+	public float getEURCHF() {
+	    return eurchf;
+	}
+	
+	public void setEURCHF(float eurchf) {
+		this.eurchf = eurchf;
+	}
+	
+	// GBP / CHF
+	public float getGBPCHF() {
+	    return gbpchf;
+	}
+	
+	public void setGBPCHF(float gbpchf) {
+		this.gbpchf = gbpchf;
+	}
+	
+	// USD / CHF
+	public float getUSDCHF() {
+	    return usdchf;
+	}
+	
+	public void setUSDCHF(float usdchf) {
+		this.usdchf = usdchf;
+	}
+	
+	// JPY / CHF
+	public float getJPYCHF() {
+	    return jpychf;
+	}
+	
+	public void setJPYCHF(float jpychf) {
+		this.jpychf = jpychf;
+	}
+	
+	// AUD / CHF
+	public float getAUDCHF() {
+	    return audchf;
+	}
+	
+	public void setAUDCHF(float audchf) {
+		this.audchf = audchf;
+	}
+	
+	// XDR / CHF
+	public float getXDRCHF() {
+	    return xdrchf;
+	}
+	
+	public void setXDRCHF(float xdrchf) {
+		this.xdrchf = xdrchf;
+	}
 }
--- src/java_assignment_2/Test.java
+++ src/java_assignment_2/Test.java
@@ -1,10 +1,63 @@
 package java_assignment_2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("111");
+	public static void main(String[] args) throws IOException {
+		
+		String row = "";
+		ArrayList<Data> data = new ArrayList<Data>();
+		
+		// Use BufferedReader to read CSV
+		boolean isHeaderLine = true;
+		BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\data.csv"));
+		try {
+			while ((row = reader.readLine()) != null) {
+				
+				// Check if line is headerline (first)
+				if(!isHeaderLine) {
+					
+					// Split line and set attributes
+					String[] line = row.split(";");
+					Data dataEntry = new Data();
+					dataEntry.setDate(line[0]);
+				    dataEntry.setEURCHF(Float.parseFloat(line[1]));
+				    dataEntry.setGBPCHF(Float.parseFloat(line[2]));
+				    dataEntry.setUSDCHF(Float.parseFloat(line[3]));
+				    dataEntry.setJPYCHF(Float.parseFloat(line[4]));
+				    dataEntry.setAUDCHF(Float.parseFloat(line[5]));
+				    dataEntry.setXDRCHF(Float.parseFloat(line[6]));
+				    
+				    // Add created object to ArrayList
+				    data.add(dataEntry);
+				} else {
+					isHeaderLine = false;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		reader.close();
+		
+		// Test data
+		for (Data dataEntry : data) {
+			System.out.println("Forex entry:");
+		    System.out.println("Date: " + dataEntry.getDate());
+		    System.out.println("EUR / CHF: " + dataEntry.getEURCHF());
+		    System.out.println("GBP / CHF: " + dataEntry.getGBPCHF());
+		    System.out.println("USD / CHF: " + dataEntry.getUSDCHF());
+		    System.out.println("JPY / CHF: " + dataEntry.getJPYCHF());
+		    System.out.println("AUD / CHF: " + dataEntry.getAUDCHF());
+		    System.out.println("XDR / CHF: " + dataEntry.getXDRCHF());
+		    System.out.println("");
+		}
+	    
 	}
 
 }
